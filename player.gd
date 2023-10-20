cdacb296ac7d4714dc6288f20b0a265814cd1211
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-10-20 22:47:10 +0100

    Add state machine animations

--- player.gd
+++ player.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 var speed := 100
 
+@onready var animation_tree := $AnimationTree
+
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
@@ -9,3 +14,10 @@ func _physics_process(delta):
 func get_input():
 	var input_direction := Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	
+	if velocity == Vector2.ZERO:
+		animation_tree.get("parameters/playback").travel("Idle")
+	else:
+		animation_tree.get("parameters/playback").travel("Walk")
+		animation_tree.set("parameters/Idle/blend_position", velocity)
+		animation_tree.set("parameters/Walk/blend_position", velocity)
