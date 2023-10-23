b4d2b3e36c36900fc716f1c288da820567c03c4f
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-10-23 17:41:33 +0100

    Add quit game on esc key press

---
+++ main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if Input.is_action_pressed("escape"):
+		get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
