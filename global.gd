bee65640aa5f10ff976f8ee3e48a28a726368fc3
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-10-24 15:11:07 +0100

    Clean up some code

--- global.gd
+++ global.gd
@@ -4,7 +4,6 @@ var current_scene = null
 var player = null
 var start_game := true
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	get_scene()
 
@@ -17,36 +16,24 @@ func get_scene():
 	current_scene = root.get_child(root.get_child_count() -1)
 	return current_scene
 
-func goto_scene(path):
+func goto_scene(path : String):
 	# Defer the scene load to a later time when no code is running
 	# in the current scene.
 	call_deferred("_deferred_goto_scene", path)
 
-func _deferred_goto_scene(path):
-	# Now safe to remove current scene
-	current_scene.free()
-	
-	# Load new scene
-	var loaded_scene = ResourceLoader.load(path)
-	
-	# Instantiate new scene
-	current_scene = loaded_scene.instantiate()
-	
-	# Add it as a child of the root
-	get_tree().root.add_child(current_scene)
-	
+func _deferred_goto_scene(path : String):
+	current_scene.free() # Now safe to remove current scene
+	var loaded_scene = ResourceLoader.load(path) # Load new scene
+	current_scene = loaded_scene.instantiate() # Instantiate new scene
+	get_tree().root.add_child(current_scene) # Add it as a child of the root
 	# (Optional) Make compatible with SceneTree.change_scene_to_file() API
 	get_tree().current_scene = current_scene
 
-func add_player_to_scene(player, position : Vector2):
-	call_deferred("_deferred_add_player_to_scene", player, position)
+func add_player_to_scene(path : String, position : Vector2):
+	call_deferred("_deferred_add_player_to_scene", path, position)
 
-func _deferred_add_player_to_scene(player, position):
-	var load_player = ResourceLoader.load(player)
+func _deferred_add_player_to_scene(path : String, position : Vector2):
+	var load_player = ResourceLoader.load(path)
 	player = load_player.instantiate()
 	get_scene().add_child(player)
 	player.position = position
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
