1fbcbb95fea3d34465e20f4f7173efcb9d75d780
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-05 19:35:28 +0000

    Fix scene path

--- scenes/main.gd
+++ scenes/main.gd
@@ -19,5 +19,5 @@ func _on_enter_building_1_body_entered(body):
 	Global.add_ui_to_scene("res://scenes/ui.tscn")
 
 func _on_enter_building_2_body_entered(body):
-	Global.goto_scene("res://scenes/house_1.tscn")
+	Global.goto_scene("res://scenes/house_2.tscn")
 	Global.add_ui_to_scene("res://scenes/ui.tscn")
